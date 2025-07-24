dfb0457852456a28402cdb5f6516ba5990006eec
Mohamed Mediouni <mohamed@unpredictable.fr>
2025-07-25 01:52:39 +0200

    hw/arm, target/arm: nested virtualisation on HVF
    Signed-off-by: Mohamed Mediouni <mohamed@unpredictable.fr>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -2383,7 +2383,8 @@ static void machvirt_init(MachineState *machine)
         exit(1);
     }
 
-    if (vms->virt && !kvm_enabled() && !tcg_enabled() && !qtest_enabled()) {
+    if (vms->virt && !kvm_enabled() && !tcg_enabled()
+       && !hvf_enabled() && !qtest_enabled()) {
         error_report("mach-virt: %s does not support providing "
                      "Virtualization extensions to the guest CPU",
                      current_accel_name());
@@ -2651,6 +2652,11 @@ static void virt_set_virt(Object *obj, bool value, Error **errp)
     VirtMachineState *vms = VIRT_MACHINE(obj);
 
     vms->virt = value;
+    /*
+     * At this point, HVF is not initialised yet.
+     * However, it needs to know if nested virt is enabled at init time.
+     */
+    hvf_nested_virt_enable(value);
 }
 
 static bool virt_get_highmem(Object *obj, Error **errp)
