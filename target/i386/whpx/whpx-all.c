7dd886cc0ce5196d57bf15a913cff9e90a4a0385
Mohamed Mediouni <mohamed@unpredictable.fr>
2026-03-23 16:53:05 +0100

    whpx: i386: add implementation for is_protected_mode helper
    Signed-off-by: Mohamed Mediouni <mohamed@unpredictable.fr>

--- target/i386/whpx/whpx-all.c
+++ target/i386/whpx/whpx-all.c
@@ -885,6 +885,13 @@ static void read_segment_descriptor(CPUState *cpu,
     }
 }
 
+static bool is_protected_mode(CPUState *cpu)
+{
+    AccelCPUState *vcpu = cpu->accel;
+
+    return vcpu->exit_ctx.VpContext.ExecutionState.Cr0Pe == 1;  
+}
+
 static bool is_long_mode(CPUState *cpu)
 {
     AccelCPUState *vcpu = cpu->accel;
@@ -930,6 +937,7 @@ static target_ulong read_cr(CPUState *cpu, int cr)
 static const struct x86_emul_ops whpx_x86_emul_ops = {
     .read_segment_descriptor = read_segment_descriptor,
     .handle_io = handle_io,
+    .is_protected_mode = is_protected_mode,
     .is_long_mode = is_long_mode,
     .is_user_mode = is_user_mode,
     .read_cr = read_cr
