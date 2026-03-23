cfa65988d3296e9a9d38a4c64ff8e25dda1f48cc
Mohamed Mediouni <mohamed@unpredictable.fr>
2026-03-23 13:47:45 +0100

    target/i386: emulate: add is_user_mode helper
    Signed-off-by: Mohamed Mediouni <mohamed@unpredictable.fr>

--- target/i386/emulate/x86_emu.h
+++ target/i386/emulate/x86_emu.h
@@ -33,6 +33,7 @@ struct x86_emul_ops {
     void (*simulate_rdmsr)(CPUState *cs);
     void (*simulate_wrmsr)(CPUState *cs);
     bool (*is_long_mode)(CPUState *cpu);
+    bool (*is_user_mode)(CPUState *cpu);
 };
 
 extern const struct x86_emul_ops *emul_ops;
